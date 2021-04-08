e834222aa269eefeb95d5513bd1a3b113e25eabd
Marek <marek.jachym@gmail.com>
2021-04-08 14:39:39 +0200

    Dodanie Background Geolocation
    podmiana App.tsx na App2.tsx

---
+++ App2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App2 = () => {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{backgroundColor: 'hotpink', padding: 40, color: 'white'}}>Podmieniłem App.tsx na App2.tsx w index.js</Text>
+        </View>
+    );
+};
+
+export default App2;
